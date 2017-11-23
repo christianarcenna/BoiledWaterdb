3771282c641d732a379031e2a308d80a9576b68e
KennethDC <kenneth.delacruz@uoit.net>
2017-11-23 18:52:24 -0500

    more comments
    here they are.

--- test.py
+++ test.py
@@ -5,18 +5,21 @@ import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
-
-
+#Joshua please correct anything if i'm wrong
+#in the response and url is the place where it gets all the data from online
+#this code currently doesnt output any file, its just for demonstration purposes on how to get data from a website/api
+#in the real code, it should have an updating json that we can use
 response = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0001/")
 url = "http://api.steampowered.com/ISteamApps/GetAppList/v0001/"
 #test = json.load(response)
 #print test
+#loads the data found here
 data = response.json()
 get = urlopen(url)
 json_object = json.load(get)
 #int(type(data))
 
-
+#url2 is just a temp url
 url2 = "http://steamsales.rhekua.com/view.php?steam_type=app&steam_id=238010"
 r = requests.get(url2)
 data = r.text
@@ -24,21 +27,44 @@ soup = BeautifulSoup(data, "html.parser")
 #steampage = BeautifulSoup(urllib2.urlopen('https://steamdb.info/app/22380/').read(), "html.parser")
 pricehistory = soup.findAll("div", { "class" : "tab_price" })
 datahistory = soup.findAll("div", { "class" : "genre_release"})
-
+#data is price history and data2 is date history. they are both list
 data = {
     'history' : [],
 }
 data2 = {
     'history2' : []
 }
+#setting variables for the price history
+tempPrice = 0.00
+prices = []
+lowestPrice = 999999999.99
+highestPrice = 0.00
+counter = 0
 for row in pricehistory:
     cols = pricehistory
-    insert = cols[0].get_text()
+    insert = cols[counter].get_text()
     indices = [pos for pos, char in enumerate(insert) if char == '$']
-    insert = insert[indices[1]: ]
-    data['history'].append( insert )
-
+    insert2 = insert[indices[0] + 1: indices[1]]
+    insert3 = insert[indices[1] + 1: ]
+    insert = insert[indices[1] + 1: ]
+    #buffers
+    tempPrice = float(insert2)
+    tempPrice2 = float(insert)
+    prices.append ( tempPrice )
+    data['history'].append( insert )#puts insert into data
+    #for the highest and lowest price
+    if tempPrice2 < lowestPrice:
+        lowestPrice = tempPrice2
+    if tempPrice > highestPrice:
+        highestPrice = tempPrice
+    counter += 1
+    #print tempPrice
+    #print tempPrice2
 print data['history'][0]
+print "Lowest Price for Deus Ex: Human Revolution: " + str(lowestPrice)
+print "Highest Price for Deus Ex: Human Revolution: " + str(highestPrice)
+print "Current Price for Deus Ex: Human Revolution: " + str(data['history'][0])
+print "Original Price for Deus Ex: Human Revolution: " + str(highestPrice)
 
 for row in datahistory:
     cols = datahistory
@@ -46,7 +72,7 @@ for row in datahistory:
 print data2['history2'][0].strip()
 
 
-#a292030 is the app id for the withcer 3 game. we are only testing for that game for now
+#the get"" stuff are variables where the things in the url are stored.
 for Apps in json_object['applist']['apps']['app']:
     if (Apps['appid'] == 292030):
         getprice = requests.get('http://store.steampowered.com/api/appdetails/?appids=%d&filters=price_overview&cc=us' % Apps['appid'])
@@ -68,7 +94,7 @@ for Apps in json_object['applist']['apps']['app']:
         #gethistory = requests.get('http://steamcommunity.com/market/pricehistory', params=params, cookies = cookie)
         #print(gethistory.text)
 
-        if getprice.status_code == 200:#status code 200 means successful fetch
+        if getprice.status_code == 200:
             rjson = json.loads(getprice.text)
             djson = json.loads(getdev.text)
             pjson = json.loads(getpub.text)
@@ -79,6 +105,9 @@ for Apps in json_object['applist']['apps']['app']:
             dlcjson = json.loads(getdlc.text)
             # use the appid to fetch the value and convert to decimal
             # appid is numeric, cast to string to lookup the price
+            # the things in the 'word' is directly from the url, where it reads the heading.
+            #example: price is a list with filters from the 'word'
+            #the except is when there is no data from it
             try:
                 price = rjson[str(Apps['appid'])]['data']['price_overview']['initial'] * .01
             except:
@@ -112,13 +141,26 @@ for Apps in json_object['applist']['apps']['app']:
                 #print dlc
             except:
                 dlc = 'N/A'
-
+            #Apps is the big list with keys and values
+            #the things in 'word' are the keys while the things after the colon are the values. which are recieved fro mthe code above.
             Apps[Apps['appid']] = {'appID': Apps['appid'],'name': Apps['name'], 'price': price, 'developer': developer, 'publisher': publisher, 'date': date, 'score': score, 'genre': genre}
-            print Apps[Apps['appid']]#this prints out all the information about the game
+            # this will print all app info (which is currently only set to one game for testing purposes)
+            # this will print the appId, name and such, in the demonstration output it is currently out of order
+            print Apps[Apps['appid']]
+            # dlcs have their own appid but steam doesnt have a "dlcid" they are treated like games so we can read them normally. this particular
+            # part of the code sets the current app list to the dlc appid.
+            # example: this is currently set to witcher 3, so this part will list all the corresponding appid of the dlc
             Apps[Apps['appid']] = {'dlc': dlc}
-            print Apps[Apps['appid']]#this will print out all the available appid for the dlc corresponding to the game
-
-#this gets the dlcs for that specific game
+            print Apps[Apps['appid']]
+            #the information for the dlc has not been printed yet at this point. it just prints out the appid of the dlc
+            """#temp stuff joshua took out
+            for dlcid in dlc:
+                Apps[Apps[dlcid]] = {'appID': dlcid, 'name': Apps['name'], 'price': price,
+                                       'developer': developer, 'publisher': publisher, 'date': date, 'score': score,
+                                       'genre': genre}
+                print Apps[Apps[dlcid]]
+            """
+#this part is the same as above now it just reads the data for specific for dlc
 for Apps in json_object['applist']['apps']['app']:
     if (Apps['appid'] in dlc):
         getprice = requests.get('http://store.steampowered.com/api/appdetails/?appids=%d&filters=price_overview&cc=us' % Apps['appid'])
@@ -176,8 +218,8 @@ for Apps in json_object['applist']['apps']['app']:
             #    history = hjson[0]
             #except:
             #    history = 'N/A'
-
+            #like above, dlcs will have the same amount of information as the apps. steam streats dlcs like games on their api
             Apps[Apps['appid']] = {'appID': Apps['appid'],'name': Apps['name'], 'price': price, 'developer': developer, 'publisher': publisher, 'date': date, 'score': score, 'genre': genre}
-            print Apps[Apps['appid']]#this prints out the dlc information
+            print Apps[Apps['appid']]#prints out all the different available dlc
 
-print(response.status_code)
+print(response.status_code)#just post of the request from the sites have been succesful
